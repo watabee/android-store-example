ade0df6fe0a0ae0cb377903ed10336fdf491a3b4
watabee <watabee.dev@gmail.com>
2020-06-13 20:12:53 +0900

    Migrate to paging3

--- app/src/main/java/com/github/watabee/storeexample/paging/DevDataSource.kt
+++ app/src/main/java/com/github/watabee/storeexample/paging/DevDataSource.kt
@@ -1,98 +1,32 @@
 package com.github.watabee.storeexample.paging
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.paging.PageKeyedDataSource
+import androidx.paging.PagingSource
 import com.dropbox.android.external.store4.Store
 import com.dropbox.android.external.store4.StoreRequest
 import com.dropbox.android.external.store4.StoreResponse
 import com.github.watabee.storeexample.api.Article
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.cancel
-import kotlinx.coroutines.cancelChildren
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.filterNot
+import kotlinx.coroutines.flow.first
 
-@OptIn(ExperimentalCoroutinesApi::class)
 class DevDataSource(
     private val tag: String,
     private val store: Store<DevConfig, List<Article>>
-) : PageKeyedDataSource<Int, Article>() {
+) : PagingSource<Int, Article>() {
 
-    private val job = Job()
-    private val scope = CoroutineScope(job)
-    private var retry: (() -> Unit)? = null
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Article> {
+        val page = params.key ?: 1
+        val config = DevConfig(page = page, perPage = params.loadSize, tag = tag)
+        val storeRequest = StoreRequest.skipMemory(config, refresh = false)
 
-    private val _networkState = MutableLiveData<NetworkState>()
-    val networkState: LiveData<NetworkState> = _networkState
+        return try {
+            val articles = store.stream(storeRequest)
+                .filterNot { it is StoreResponse.Loading }
+                .first()
+                .requireData()
 
-    override fun loadInitial(params: LoadInitialParams<Int>, callback: LoadInitialCallback<Int, Article>) {
-        job.cancelChildren()
-        store.stream(StoreRequest.skipMemory(DevConfig(page = 1, perPage = params.requestedLoadSize, tag = tag), refresh = false))
-            .onEach { storeResponse ->
-                when (storeResponse) {
-                    is StoreResponse.Loading -> {
-                        _networkState.postValue(NetworkState.Loading(true))
-                    }
-                    is StoreResponse.Data -> {
-                        retry = null
-                        _networkState.postValue(NetworkState.Loaded(true))
-                        callback.onResult(storeResponse.value, null, 2)
-                    }
-                    is StoreResponse.Error -> {
-                        retry = {
-                            loadInitial(params, callback)
-                        }
-                        _networkState.postValue(NetworkState.Error(true, storeResponse.errorMessageOrNull().orEmpty()))
-                    }
-                }
-            }
-            .launchIn(scope)
-    }
-
-    override fun loadBefore(params: LoadParams<Int>, callback: LoadCallback<Int, Article>) {
-        // do nothing.
-    }
-
-    override fun loadAfter(params: LoadParams<Int>, callback: LoadCallback<Int, Article>) {
-        job.cancelChildren()
-        val page = params.key
-        store.stream(StoreRequest.skipMemory(DevConfig(page = page, perPage = params.requestedLoadSize, tag = tag), refresh = false))
-            .onEach { storeResponse ->
-                when (storeResponse) {
-                    is StoreResponse.Loading -> {
-                        _networkState.postValue(NetworkState.Loading(false))
-                    }
-                    is StoreResponse.Data -> {
-                        retry = null
-                        _networkState.postValue(NetworkState.Loaded(false))
-                        callback.onResult(storeResponse.value, page + 1)
-                    }
-                    is StoreResponse.Error -> {
-                        retry = {
-                            loadAfter(params, callback)
-                        }
-                        _networkState.postValue(NetworkState.Error(false, storeResponse.errorMessageOrNull().orEmpty()))
-                    }
-                }
-            }
-            .launchIn(scope)
-    }
-
-    override fun invalidate() {
-        super.invalidate()
-        cancel()
-    }
-
-    fun retry() {
-        val prevRetry = retry
-        retry = null
-        prevRetry?.invoke()
-    }
-
-    fun cancel() {
-        scope.cancel()
+            LoadResult.Page(data = articles, prevKey = null, nextKey = page + 1)
+        } catch (e: Throwable) {
+            LoadResult.Error(e)
+        }
     }
 }
--- app/src/main/java/com/github/watabee/storeexample/paging/NetworkState.kt
+++
@@ -1,7 +0,0 @@
-package com.github.watabee.storeexample.paging
-
-sealed class NetworkState {
-    class Loading(val isInitial: Boolean) : NetworkState()
-    class Loaded(val isInitial: Boolean) : NetworkState()
-    class Error(val isInitial: Boolean, val message: String): NetworkState()
-}
