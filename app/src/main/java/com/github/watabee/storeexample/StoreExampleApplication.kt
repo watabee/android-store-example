492e6157601fe2622c4d09c9e5edf35d295074a5
watabee <watabee.dev@gmail.com>
2020-06-14 13:02:24 +0900

    Introduce App Startup

--- app/src/main/java/com/github/watabee/storeexample/StoreExampleApplication.kt
+++ app/src/main/java/com/github/watabee/storeexample/StoreExampleApplication.kt
@@ -2,16 +2,6 @@ package com.github.watabee.storeexample
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
-import timber.log.Timber
 
 @HiltAndroidApp
-class StoreExampleApplication : Application() {
-
-    override fun onCreate() {
-        super.onCreate()
-
-        if (BuildConfig.DEBUG) {
-            Timber.plant(Timber.DebugTree())
-        }
-    }
-}
+class StoreExampleApplication : Application()
---
+++ app/src/main/java/com/github/watabee/storeexample/initializer/TimberInitializer.kt
@@ -0,0 +1,16 @@
+package com.github.watabee.storeexample.initializer
+
+import android.content.Context
+import androidx.startup.Initializer
+import com.github.watabee.storeexample.BuildConfig
+import timber.log.Timber
+
+class TimberInitializer : Initializer<Unit> {
+    override fun create(context: Context) {
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+
+    override fun dependencies(): List<Class<out Initializer<*>>> = emptyList()
+}
